9064e4c8ac953ea77093bf467fd0046996dca234
DimaPth <dzmitry.pth@mail.ru>
2023-03-12 00:22:17 +0300

    add redux persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 
+import { SignUp } from './components/auth/registration';
+import { Auth } from './components/auth/auth';
+import { ForgotPass } from './components/auth/forgot-pass';
 import { BookLayout } from './components/layout/book-layout';
 import { Layout } from './components/layout/layout';
 import { BookPage } from './pages/book';
 import { ContractPage } from './pages/contract';
 import { MainPage } from './pages/main';
 import { RulesPage } from './pages/rules';
-import {store} from './redux/store'
+import { store, persistor } from './redux/store'
 
 import './index.css';
 
@@ -18,19 +22,24 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
-          <Routes>
-            <Route path='/' element={<Layout />}>
-              <Route index={true} element={<MainPage />}/>
-              <Route path='/books/:category' element={<MainPage />}/>
-              <Route path='/rules' element={<RulesPage />}/>
-              <Route path='/contract' element={<ContractPage />}/>
-            </Route>
-            <Route path='/books/:category/:bookId' element={<BookLayout />}>
-              <Route index={true} element={<BookPage />}/>
-            </Route>
-          </Routes>
-      </HashRouter>
+      <PersistGate persistor={persistor}>
+        <HashRouter>
+            <Routes>
+              <Route path='/' element={<Layout />}>
+                <Route index={true} element={<MainPage />}/>
+                <Route path='/books/:category' element={<MainPage />}/>
+                <Route path='/rules' element={<RulesPage />}/>
+                <Route path='/contract' element={<ContractPage />}/>
+              </Route>
+              <Route path='/books/:category/:bookId' element={<BookLayout />}>
+                <Route index={true} element={<BookPage />}/>
+              </Route>
+              <Route path='/auth' element={<Auth />}/>
+              <Route path='/registration' element={<SignUp />}/>
+              <Route path='/forgot-pass' element={<ForgotPass />}/>
+            </Routes>
+        </HashRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,47 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { 
+  persistStore, 
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER, 
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import { menuSlice } from './slices/menu-slice'
 import { booksApi } from './books-api'
+import { authApi } from './auth-api'
+import { userSlice } from './slices/user-slice'
+
+const rootReducer = combineReducers({
+  menu: menuSlice.reducer,
+  user: userSlice.reducer,
+  [booksApi.reducerPath]: booksApi.reducer,
+  [authApi.reducerPath]: authApi.reducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: [booksApi.reducerPath, authApi.reducerPath]
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    menu: menuSlice.reducer,
-    [booksApi.reducerPath]: booksApi.reducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(booksApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(booksApi.middleware, authApi.middleware),
 })
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
